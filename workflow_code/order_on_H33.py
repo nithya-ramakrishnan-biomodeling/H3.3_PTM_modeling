ea5140bf7e4a559787bf36c6907f46efe946551f
nehabachineni <56502673+nehabachineni@users.noreply.github.com>
2026-04-06 15:04:41 +0530

    Update order_on_H33.py, docstring

--- workflow_code/order_on_H33.py
+++ workflow_code/order_on_H33.py
@@ -7,10 +7,25 @@ Reorders common genes according to H3.3 BED file order.
 def run(input_common_genes, h3_bed_file, output_file):
     """
     Parameters:
-        input_common_genes : path to common genes file
-        h3_bed_file        : path to H3.3 BED file
-        output_file        : path to save reordered file
+        input_common_genes : path to common genes file     sample ex: common_genes.txt
+        h3_bed_file        : path to H3.3 BED file         sample ex: H3.3_CT0.bed
+        output_file        : path to save reordered file   sample ex: ordered_genes.txt
+
+        Output Snippet (head -10 ordered_genes.txt)
+
+        2	NR_046233
+        3	NM_177780
+        4	NM_001362405
+        5	NR_155555
+        6	NM_001362404
+        7	NM_178640
+        9	NM_001164256
+        10	NM_001164252
+        11	NM_001164254
+        12	NM_001164253
+
     """
+    
     # Reading common genes
     with open(input_common_genes, 'r') as f:
         common_genes = set(line.strip() for line in f)
